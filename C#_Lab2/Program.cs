90f12c71703c7b1de4137a00c4920e9af63ec033
okamodgi <nadakupnada@gmail.com>
2024-02-13 16:58:58 +0300

    без метода

--- C#_Lab2/Program.cs
+++ C#_Lab2/Program.cs
@@ -1,17 +1,13 @@
-﻿void Matem()
+﻿Console.WriteLine("Впишите N");
+int N = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Впишите X");
+int X = Convert.ToInt32(Console.ReadLine());
+double res = -Math.PI / 2;
+for (
+    int i = 0; i <= N; i++
+    )
 {
-    Console.WriteLine("Впишите N");
-    int N = Convert.ToInt32(Console.ReadLine());
-    Console.WriteLine("Впишите X");
-    int X = Convert.ToInt32(Console.ReadLine());
-    double res = -Math.PI / 2;
-    for (
-        int i = 0; i <= N; i++
-        )
-    {
-        res = res + ((Math.Pow((-1), (i + 1))) / (((2 * i + 1) * (Math.Pow(X, (2 * i + 1))))));
-    }
-    Console.WriteLine(res);
-
+    res = res + ((Math.Pow((-1), (i + 1))) / (((2 * i + 1) * (Math.Pow(X, (2 * i + 1))))));
 }
+Console.WriteLine(res);
 
